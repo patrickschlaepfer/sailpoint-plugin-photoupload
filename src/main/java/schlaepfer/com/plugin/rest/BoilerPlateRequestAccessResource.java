81a44ef0e2ef39b2295536d44d02cb9ddaa856b4
Patrick Schlaepfer <patrick.schlaepfer@ic-consult.ch>
2017-08-29 10:45:13 +0200

    Added some examples for REST services

--- src/main/java/schlaepfer/com/plugin/rest/BoilerPlateRequestAccessResource.java
+++ src/main/java/schlaepfer/com/plugin/rest/BoilerPlateRequestAccessResource.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import sailpoint.integration.ListResult;
+import sailpoint.integration.RequestResult;
 import sailpoint.rest.plugin.AllowAll;
 import sailpoint.rest.plugin.BasePluginResource;
 import sailpoint.tools.GeneralException;
@@ -15,16 +20,52 @@ import sailpoint.tools.GeneralException;
 public class BoilerPlateRequestAccessResource extends BasePluginResource {
 
 	private static final String PLUGIN_NAME = "BoilerplatePlugin";
+	private static Log logger = LogFactory.getLog(BoilerPlateRequestAccessResource.class);
 
 	@GET
 	@Path("identities")
-	@Consumes("application/vnd.boilderplate-v1+json")
+	@Consumes("application/vnd.boilerplate-v1+json")
 	@AllowAll
 	public ListResult getIdentities() throws GeneralException {
 		List newList = new ArrayList<>();
+		ListResult retResult = null;
 		
-		return new ListResult(newList, newList.size());
+		try {
+		
+			retResult = new ListResult(newList, newList.size());
+			
+		} catch (Throwable thr) {
+            logger.error("Exception while getting non personal accounts");
+            retResult = new ListResult(nonPersonalAccountDTOs, nonPersonalAccountDTOs.size());
+            retResult.setStatus(RequestResult.STATUS_FAILURE);
+            retResult.addError(thr);
+		}	
+		return retResult;
 	}
+	
+	@POST
+    @Consumes("application/vnd.boilerplate-v1+json")
+    @Path("getIdentites")
+    @AllowAll
+    public ListResult getSelectedIdentites(Map<String, Object> request) throws GeneralException {
+           ListResult retResult = null;
+           IdentitesSearchDTO identitesSearchDTO = null;
+           List<IdentityDTO> identitesDTO = new ArrayList<>();
+           
+           ObjectMapper mapper = new ObjectMapper();
+           try {
+                  identitesSearchDTO = mapper.convertValue(request, IdentitesSearchDTO.class);
+                  
+           } catch (Throwable thr) {
+               logger.error("Exception while getSelectedIdentites", thr);
+               retResult = new ListResult(identitesDTO, identitesDTO.size());
+               retResult.addError(thr);
+           }
+           
+           return retResult;
+           
+	}
+	
 
 	@Override
 	public String getPluginName() {
