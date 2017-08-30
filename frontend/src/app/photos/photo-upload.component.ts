f0317b92f28790d82660240ccc40b6c6a387f49f
Patrick Schlaepfer <patrick.schlaepfer@ic-consult.ch>
2017-08-30 14:44:15 +0200

    Added additional field to form

--- frontend/src/app/photos/photo-upload.component.ts
+++ frontend/src/app/photos/photo-upload.component.ts
@@ -3,6 +3,24 @@ export interface IPhotoUploadController {
 
 export class PhotoUploadController implements  IPhotoUploadController {
     static controllerId = 'PhotoUploadController';
+    
+    public dzOptions:any = {
+    		url : '/alt_upload_url',
+    		paramName : 'photo',
+    		maxFilesize : '10',
+    		maxFiles: '1',
+    		acceptedFiles : 'image/jpeg, images/jpg, image/png',
+    		addRemoveLinks : true,
+    		dictDefaultMessage : 'Click to add or drop photo',
+    		autoProcessQueue:false
+    }
+    
+    
+    public dzCallbacks:any = {
+    		'addedfile': function(file) {
+    			console.log(file);
+    		}
+    }
    
     static $inject: Array<string> = [
         '$log'];
@@ -10,4 +28,6 @@ export class PhotoUploadController implements  IPhotoUploadController {
     	
     }
 
+    
+    
 }
